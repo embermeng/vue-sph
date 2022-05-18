d23e93d16840464e1e4088a73b012a0f9d11d8d9
menglxa <menglxa@digitalchina.com>
2022-05-18 17:55:18 +0800

    到33p

--- src/main.js
+++ src/main.js
@@ -7,12 +7,12 @@ Vue.config.productionTip = false
 import TypeNav from '@/components/TypeNav'
 // 第一个参数：全局组件名字 第二个参数： 哪一个组件
 Vue.component(TypeNav.name, TypeNav)
-
 // 引入路由
 import router from '@/router'
 // 引入仓库
 import store from '@/store'
-
+// 引入MockServe.js
+import '@/mock/mockServe'
 new Vue({
     render: h => h(App),
     // 注册路由
